15cd6607a679689aafb31acc24e9dacc6c97d598
jeremy <jeremy@chaufourier.fr>
2025-04-26 00:43:55 +0200

    feat: emit event in terminal

---
+++ start/events.ts
@@ -0,0 +1,26 @@
+// https://docs.adonisjs.com/guides/references/events
+import string from '@adonisjs/core/helpers/string'
+import app from '@adonisjs/core/services/app'
+import emitter from '@adonisjs/core/services/emitter'
+import logger from '@adonisjs/core/services/logger'
+import db from '@adonisjs/lucid/services/db'
+
+if (app.inDev) {
+  emitter.on('db:query', db.prettyPrint)
+
+  emitter.on('http:request_completed', (event) => {
+    const method = event.ctx.request.method()
+    const url = event.ctx.request.url(true)
+    const duration = event.duration
+
+    logger.info(`${method} ${url}: ${string.prettyHrTime(duration)}`)
+  })
+
+  // emitter.on('i18n:missing:translation', function (event) {
+  //   logger.warn(`Translation missing: ${event.locale}, ${event.identifier}`)
+  // })
+
+  // emitter.on('mail:sending', (event) => {
+  //   logger.info(`Mail ${event.mailerName} is sending: ${event.message.subject}`)
+  // })
+}
