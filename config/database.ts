de18bf6b06aa256c2e757cb3cfdc89aa4c6917da
jeremy <jeremy@chaufourier.fr>
2025-08-07 17:29:06 +0200

    feat: migrate sqlite -> pgsql

--- config/database.ts
+++ config/database.ts
@@ -1,23 +1,9 @@
-import app from '@adonisjs/core/services/app'
 import { defineConfig } from '@adonisjs/lucid'
 import env from '#start/env'
 
 const dbConfig = defineConfig({
   connection: env.get('DB_CONNECTION'),
   connections: {
-    sqlite: {
-      client: 'better-sqlite3',
-      connection: {
-        filename: app.tmpPath('db.sqlite3'),
-      },
-      useNullAsDefault: true,
-      migrations: {
-        naturalSort: true,
-        paths: ['database/migrations'],
-        disableRollbacksInProduction: true,
-      },
-      debug: app.inDev,
-    },
     postgres: {
       client: 'pg',
       connection: {
