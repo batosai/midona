fdec4a8ba5308f11f726bc839246d59c7c514647
jeremy <jeremy@chaufourier.fr>
2025-08-07 17:29:39 +0200

    chore: change baseUrl

--- config/transmit.ts
+++ config/transmit.ts
@@ -3,4 +3,5 @@ import { defineConfig } from '@adonisjs/transmit'
 export default defineConfig({
   pingInterval: false,
   transport: null,
+  baseUrl: 'http://127.0.0.1:3333',
 })
--- inertia/settings/tuyau.ts
+++ inertia/settings/tuyau.ts
@@ -3,5 +3,5 @@ import { api } from 'midona/api'
 
 export const tuyau = createTuyau({
   api,
-  baseUrl: 'http://localhost:3333',
+  baseUrl: 'http://127.0.0.1:3333',
 })
