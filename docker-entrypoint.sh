cf7341ad71d33bb7ab89880f3e0c17b2f41db955
jeremy <jeremy@chaufourier.fr>
2025-08-07 23:54:41 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,7 @@
 
 node ace migration:run --force
 
-node ace db:user:create --firstname="$ADMIN_FIRSTNAME" --lastname="$ADMIN_LASTNAME" --email="$ADMIN_EMAIL" --password="$ADMIN_PASSWORD"
+if [ ! -f tmp/first_run ]; then
+  node ace db:user:create --firstname="$ADMIN_FIRSTNAME" --lastname="$ADMIN_LASTNAME" --email="$ADMIN_EMAIL" --password="$ADMIN_PASSWORD"
+  touch tmp/first_run
+fi
