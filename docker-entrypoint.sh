fe94c5720fd7a5238999b6553937676e9d1b3b78
jeremy <jeremy@chaufourier.fr>
2025-08-09 22:13:37 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,3 +6,5 @@ if [ ! -f tmp/first_run ]; then
   node ace db:user:create --firstname="$ADMIN_FIRSTNAME" --lastname="$ADMIN_LASTNAME" --email="$ADMIN_EMAIL" --password="$ADMIN_PASSWORD"
   touch tmp/first_run
 fi
+
+exec "$@"
