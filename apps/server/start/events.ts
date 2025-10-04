dccb3831d89297b23e7860d860baf2d820b83db4
Jeremy Chaufourier <jeremy@chaufourier.fr>
2025-10-04 11:34:48 +0200

    chore(server): add event access_tokens

--- apps/server/start/events.ts
+++ apps/server/start/events.ts
@@ -16,9 +16,34 @@ if (app.inDev) {
     logger.info(`${method} ${url}: ${string.prettyHrTime(duration)}`)
   })
 
-  // emitter.on('i18n:missing:translation', function (event) {
-  //   logger.warn(`Translation missing: ${event.locale}, ${event.identifier}`)
-  // })
+  emitter.on('access_tokens_auth:authentication_attempted', (event) => {
+    console.log(event.guardName)
+  })
+
+
+  emitter.on('access_tokens_auth:authentication_succeeded', (event) => {
+    console.log(event.guardName)
+    console.log(event.user)
+    console.log(event.token)
+  })
+
+  emitter.on('access_tokens_auth:authentication_failed', (event) => {
+    console.log(event.guardName)
+    console.log(event.error)
+  })
+
+  /*emitter.on('authorization:finished', (event) => {
+    console.log(event.user)
+    console.log(event.response)
+    console.log(event.parameters)
+    console.log(event.action)
+  })*/
+
+
+
+  emitter.on('i18n:missing:translation', function (event) {
+    logger.warn(`Translation missing: ${event.locale}, ${event.identifier}`)
+  })
 
   // emitter.on('mail:sending', (event) => {
   //   logger.info(`Mail ${event.mailerName} is sending: ${event.message.subject}`)
