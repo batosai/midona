b358c384ea167391c50cc2d6fc48e488f4c2f451
Jeremy Chaufourier <jeremy@chaufourier.fr>
2025-10-21 00:44:25 +0200

    feat(server): add file entity

---
+++ apps/server/database/migrations/1760992542863_create_files_table.ts
@@ -0,0 +1,25 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'files'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().notNullable().index()
+
+      table.string('name', 255).notNullable()
+      table.string('mime_type', 255)
+      table.integer('size')
+      table.json('file_data').notNullable()
+
+      table.uuid('content_id').references('contents.id').onDelete('CASCADE')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
